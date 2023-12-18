fca20df9972bf45c355c1b8b58e1bd5a8b5ff87e
mic050r <103114387+mic050r@users.noreply.github.com>
2023-12-18 19:52:15 +0900

    /bye 페이지 만들기

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -6,13 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping; // URL 연결 요청(
 
 
 
-@Controller // 컨트롤러 선언
+@Controller // 1. 컨트롤러 선언
 public class FirstController {
 
-    @GetMapping("/hi") // URL 요청 접수
-    public String niceToMeetYou(Model model) { // model 객체 받아오기
+    @GetMapping("/hi") // 2. URL 요청 접수
+    // 3. 메소드 수행
+    public String niceToMeetYou(Model model) { // 4. model 객체 가져오기
         // 모델에서 변수를 등록할 때는 형식 : model.addAttribute("변수명", 변숫값)
-        model.addAttribute("username", "홍광호");
-        return "greetings"; // 서버가 알아서 greetings.mustache로 파일 변환
+        model.addAttribute("username", "홍광호"); // 5. 모델 변수 등록
+        return "greetings"; // 6. 뷰 템플릿 페이지 변환
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "홍광호");
+        return "goodbye";
     }
 }
