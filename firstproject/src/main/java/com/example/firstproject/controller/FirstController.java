444ab8b68314b523a86ffea4730b67f325c536d4
mic050r <103114387+mic050r@users.noreply.github.com>
2023-12-18 19:40:56 +0900

    모델 추가하여 변수명 수정

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; // Model 클래스 패키지
 import org.springframework.web.bind.annotation.GetMapping; // URL 연결 요청(@GetMapping())
 
 
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping; // URL 연결 요청(
 public class FirstController {
 
     @GetMapping("/hi") // URL 요청 접수
-    public String niceToMeetYou() { // 메소드 작성
+    public String niceToMeetYou(Model model) { // model 객체 받아오기
+        // 모델에서 변수를 등록할 때는 형식 : model.addAttribute("변수명", 변숫값)
+        model.addAttribute("username", "홍광호");
         return "greetings"; // 서버가 알아서 greetings.mustache로 파일 변환
     }
 }
